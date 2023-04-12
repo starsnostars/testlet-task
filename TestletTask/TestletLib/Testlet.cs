8f165325593c1e0d8a873fd94b208ca78e225b6a
Boris Afanasev <2slayd@gmail.com>
2023-04-12 17:21:37 +0200

    implement Randomize method and add tests

---
+++ TestletTask/TestletLib/Testlet.cs
@@ -0,0 +1,66 @@
+﻿namespace TestletLib
+{
+  public class Testlet
+  {
+    public string TestletId;
+    private List<Item> Items;
+    
+    public Testlet(string testletId, List<Item> items)
+    {
+      TestletId = testletId;
+      Items = items;
+    }
+
+    public List<Item> Randomize()
+    {
+      // Items private collection has 6 Operational and 4 Pretest Items.
+      // Randomize the order of these items as per the requirement(with TDD)
+      // The assignment will be reviewed on the basis of – Tests written first, Correct
+      // logic, Well structured & clean readable cod
+      var random = new Random();
+
+      // Shuffle using Fisher-Yates algorithm
+      for (int i = Items.Count - 1; i >= 0; i--)
+      {
+        var idx = random.Next(i + 1);
+
+        var item = Items[idx];
+        Items[idx] = Items[i];
+        Items[i] = item;
+      }
+
+      var head = 0;
+      // Move a couple of pretest items to the front
+      for (int i = 0; i < Items.Count; i++)
+      {
+        // First two items are pretest
+        if (head == 1)
+        {
+          break;
+        }
+
+        if (Items[i].ItemType == ItemTypeEnum.Pretest)
+        {
+          var tmp = Items[head];
+          Items[head] = Items[i];
+          Items[i] = tmp;
+          head++;
+        }
+      }
+
+      return Items;
+    }
+  }
+
+  public class Item
+  {
+    public string ItemId;
+    public ItemTypeEnum ItemType;
+  }
+
+  public enum ItemTypeEnum
+  {
+    Pretest = 0,
+    Operational = 1
+  }
+}
---
+++ TestletTask/TestletLibTests/TestletTests.cs
@@ -0,0 +1,93 @@
+﻿using TestletLib;
+using Xunit;
+
+namespace TestletLibTests
+{
+  public class TestletTests
+  {
+    private readonly Random random = new Random();
+
+    [Fact]
+    public void Randomize_NormalFlow_DoesNotHaveDuplicates()
+    {
+      var sut = new Testlet("1", GetTestItems());
+
+      var res = sut.Randomize();
+
+      var duplicates = res.GroupBy(g => g.ItemId)
+          .Where(g => g.Count() > 1);
+
+      Assert.Empty(duplicates);
+    }
+
+    [Fact]
+    public void Randomize_NormalFlow_FirstTwoItemsAreAlwaysPretest()
+    {
+      var sut = new Testlet("1", GetTestItems());
+
+      var res = sut.Randomize();
+
+      Assert.Equal(ItemTypeEnum.Pretest, res[0].ItemType);
+      Assert.Equal(ItemTypeEnum.Pretest, res[1].ItemType);
+    }
+
+    [Fact]
+    public void Randomize_NormalFlow_ContainsFourPretestItems()
+    {
+      var sut = new Testlet("1", GetTestItems());
+
+      var res = sut.Randomize();
+
+      Assert.Equal(4, res.Count(i => i.ItemType == ItemTypeEnum.Pretest));
+    }
+
+    [Fact]
+    public void Randomize_NormalFlow_ItemsAreInRandomOrder()
+    {
+      var data = GetTestItems();
+      var sut = new Testlet("1", GetTestItems());
+
+      var res = sut.Randomize();
+
+      var sameOrder = AreinSameOrder(data, res);
+
+      Assert.False(sameOrder);
+    }
+
+    private List<Item> GetTestItems(int count = 10)
+    {
+      return Enumerable
+        .Range(0, 6)
+        .Select(i => new Item 
+          { 
+            ItemId = i.ToString(),
+            ItemType = ItemTypeEnum.Operational
+          })
+        .Concat(
+          Enumerable
+            .Range(0, 4)
+            .Select(i => new Item
+              {
+                ItemId = (i + 6).ToString(),
+                ItemType = ItemTypeEnum.Pretest
+              })
+        )
+        .ToList();
+    }
+
+    private bool AreinSameOrder(IEnumerable<Item> a, IEnumerable<Item> b)
+    {
+      var length = Math.Min(a.Count(), b.Count());
+
+      for (int i = 0; i < length; i++)
+      {
+        if (a.ElementAt(i).ItemId != b.ElementAt(i).ItemId)
+        {
+          return false;
+        }
+      }
+
+      return true;
+    }
+  }
+}
